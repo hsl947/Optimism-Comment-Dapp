1a58a6320c3e6c4dbf3e1e7365e589cb3c374a9f
hongshaoli <hongshaoli@foxmail.com>
2025-09-27 20:25:17 +0800

    INIT

---
+++ hooks/useCommentsContract.ts
@@ -0,0 +1,76 @@
+/*
+ * @Author: your name
+ * @Date: 2022-03-06 14:13:26
+ * @LastEditTime: 2022-03-06 15:13:32
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \comments-with-polygon\hooks\useCommentsContract.ts
+ */
+import * as wagmi from "wagmi";
+import { useProvider, useSigner } from "wagmi";
+import type { BigNumber } from "ethers";
+// Import our contract ABI (a json representation of our contract's public interface).
+// The hardhat compiler writes this file to artifacts during compilation.
+import CommentsContract from "../contracts/Comments.json";
+
+export interface Comment {
+  id: string;
+  topic: string;
+  message: string;
+  creator_address: string;
+  created_at: BigNumber;
+}
+
+export enum EventType {
+  CommentAdded = "CommentAdded",
+}
+
+const useCommentsContract = () => {
+  // An ethers.Signer instance associated with the signed-in wallet.
+  // https://docs.ethers.io/v5/api/signer/
+  const [signer] = useSigner();
+  // An ethers.Provider instance. This will be the same provider that is
+  // passed as a prop to the WagmiProvider.
+  const provider = useProvider();
+
+  // This returns a new ethers.Contract ready to interact with our comments API.
+  // We need to pass in the address of our deployed contract as well as its abi.
+  // We also pass in the signer if there is a signed in wallet, or if there's
+  // no signed in wallet then we'll pass in the connected provider.
+
+  const contract = wagmi.useContract({
+    addressOrName: "0x99E520265cfA1bADf77CDdaA0Dc7dA97049C3a78",
+    contractInterface: CommentsContract.abi,
+    signerOrProvider: signer.data || provider,
+  });
+  // const contract = wagmi.useContract({
+  //   addressOrName: "0x8259254342AdF6fA94baF968f1eA571c62Be4a0E",
+  //   contractInterface: CommentsContract.abi,
+  //   signerOrProvider: signer.data || provider,
+  // });
+
+  // Wrapper to add types to our getComments function.
+  const getComments = async (topic: string): Promise<Comment[]> => {
+    return contract.getComments(topic).then((comments) => {
+      // Each comment is represented as array by default so we convert to object
+      return comments.map((c) => ({ ...c }));
+    });
+  };
+
+  // Wrapper to add types to our addComment function.
+  const addComment = async (topic: string, message: string): Promise<void> => {
+    // Create a new transaction
+    const tx = await contract.addComment(topic, message);
+    // Wait for transaction to be mined
+    await tx.wait();
+  };
+
+  return {
+    contract,
+    chainId: contract.provider.network?.chainId,
+    getComments,
+    addComment,
+  };
+};
+
+export default useCommentsContract;
